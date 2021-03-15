610974d7a50a0f0da24e1656430864fe0c3f3053
hayami226 <59946552+hayami226@users.noreply.github.com>
2021-03-15 22:01:09 +0900

    Initial Commit

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ const handleHome = (req, res) => res.send("Hello from my ass");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+//middleware(★순서 중요★)
+//모든 라우터에 적용 : app.use(betweenHome);
+//하나의 라우터에 적용 : app.get("/", betweenHome, handleHome);
+app.use(betweenHome);
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 
