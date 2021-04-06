ecc7d1a90653eb58e56a4ba7f5d2e89b25b198c4
hayami226 <59946552+hayami226@users.noreply.github.com>
2021-04-06 21:02:43 +0900

    20210406 Commit

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,15 +1,16 @@
 import express from "express";
 import { getJoin, postJoin, getLogin, postLogin, logout } from "../controllors/userControllor";
 import { home, search } from "../controllors/videoControllor";
-import routes from "../routes"
+import routes from "../routes";
+import { onlyPublic } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin, postLogin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
