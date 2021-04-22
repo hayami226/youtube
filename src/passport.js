80b59c0c4cd73567c409e25b334b5b967812a2ef
hayami226 <hayami226@naver.com>
2021-04-22 12:41:17 +0900

    commit

--- src/passport.js
+++ src/passport.js
@@ -14,7 +14,7 @@ passport.use(
         clientID: process.env.GH_ID,
         clientSecret: process.env.GH_SECRET,
         callbackURL: process.env.PRODUCTION
-        ? `https://polar-sea-27980.herokuapp.com${routes.githubCallback}`
+        ? `https://shrouded-wave-62913.herokuapp.com${routes.githubCallback}`
         : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
