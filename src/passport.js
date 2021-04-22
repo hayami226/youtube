09bdd5f6bbd08e5c5f466ad72bc4b0c65b2bba33
hayami226 <hayami226@naver.com>
2021-04-22 21:14:12 +0900

    20210422

--- src/passport.js
+++ src/passport.js
@@ -14,8 +14,8 @@ passport.use(
         clientID: process.env.GH_ID,
         clientSecret: process.env.GH_SECRET,
         callbackURL: process.env.PRODUCTION
-        ? `https://shrouded-wave-62913.herokuapp.com${routes.githubCallback}`
-        : `http://localhost:4000${routes.githubCallback}`
+         ? `https://shrouded-wave-62913.herokuapp.com${routes.githubCallback}`
+         : `http://localhost:4000${routes.githubCallback}`
     },
     githubLoginCallback
     )
